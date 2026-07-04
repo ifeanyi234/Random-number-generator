4d97657a104dd8f7628d5020fbb530eef5a27882
sabzyhacks <ei711283@gmail.com>
2026-07-04 10:46:51 +0100

    Refactored and fixed bug in condition

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ const generateBtn = document.querySelector("#generateBtn");
 const errorMsg = document.querySelector(".error");
 const outputNum = document.querySelector("#number");
 
+// Generate Random Number
+const generateNum = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// Display Random Number
+const displayNum = function (num) {
+  outputNum.textContent = num;
+};
+
 generateBtn.addEventListener("click", function (e) {
   e.preventDefault();
 
-  const minValue = +minInput.value; // i decided to use this instead of parseInt, tell me is this better or worse
-  const maxValue = +maxInput.value;
+  const minValue = Number(minInput.value);
+  const maxValue = Number(maxInput.value);
 
   // Validate Numbers
-  if ((maxValue && minValue) === 0) {
+  if (!minInput.value || !maxInput.value) {
     errorMsg.textContent = "Please input min & max values before generating!";
     errorMsg.classList.add("active");
   } else if (minValue >= maxValue) {
@@ -20,17 +30,9 @@ generateBtn.addEventListener("click", function (e) {
     errorMsg.classList.add("active");
   } else {
     errorMsg.classList.remove("active");
-
-    // Generate Random Number
-    const generateNum = function (min, max) {
-      return Math.floor(Math.random() * (max - min) + min);
-    };
+    // Call function
     const randomNum = generateNum(minValue, maxValue);
-
-    // Display Random Number
-    const displayNum = function (num) {
-      outputNum.textContent = num;
-    };
+    // Call function
     displayNum(randomNum);
   }
 });
